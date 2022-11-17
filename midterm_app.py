b159d658a4a8dcfdfb1b9ba7ebf97d99f17ad40f
qjfcaseo <qjfcaseo@tip.edu.ph>
2022-11-17 04:03:29 +0000

    final version q27

--- midterm_app.py
+++ midterm_app.py
@@ -6,7 +6,7 @@ sample = Flask(__name__)
 
 @sample.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=5050)
